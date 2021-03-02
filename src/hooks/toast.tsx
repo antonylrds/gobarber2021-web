1c53c2df8ffe2279f81096f4665689b1b753de29
antonylrds <42950733+antonylrds@users.noreply.github.com>
2021-03-02 20:25:14 -0300

    Pacote uuid utilizado invés de uuidv4(depreciado)

--- src/hooks/toast.tsx
+++ src/hooks/toast.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useCallback, useState } from 'react';
 
-import { uuid } from 'uuidv4';
+import { v4 } from 'uuid';
 
 import ToastContainer from '../components/ToastContainer';
 
@@ -23,7 +23,7 @@ const ToastProvider: React.FC = ({ children }) => {
 
   const addToast = useCallback(
     ({ type, title, description }: Omit<ToastMessage, 'id'>) => {
-      const id = uuid();
+      const id = v4();
 
       const toast = {
         id,
